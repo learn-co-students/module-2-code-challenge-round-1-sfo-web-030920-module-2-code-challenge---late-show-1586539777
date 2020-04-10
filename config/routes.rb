a179168d547fafa9441f6238d50e9b565cd43b5e
lalogaribo <garibo.eduardo90@gmail.com>
2020-04-10 11:53:17 -0700

    Completed first part

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :appearances
-  resources :guest_episodes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :guests
   resources :episodes
 end
