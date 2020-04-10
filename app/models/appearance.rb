ef15677a6a8b5cfadaf33b4af36c0a550883b2f6
caicindy87 <caicindy87@gmail.com>
2020-04-10 11:43:52 -0700

    Did 1, 2, 3, 5, and some of 4

---
+++ app/models/appearance.rb
@@ -0,0 +1,5 @@
+class Appearance < ApplicationRecord
+  belongs_to :guest
+  belongs_to :episode
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,9 @@
 class Episode < ApplicationRecord
+    has_many :appearances
+    has_many :guests, through: :appearances
+
+
+    def ratings
+        self.appearances.map {|appearance| appearance.rating}
+    end
 end
